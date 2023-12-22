026d35f18b672e8dea52f7a63f7ab81811ceecbd
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-21 21:39:30 -0800

    Add theme settings to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,40 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                jetBrainsMono: ["JetBrains Mono"],
+            },
+            content: {
+                rightArrow: 'url("../assets/images/icon-arrow-right.svg")',
+            },
+        },
+        colors: {
+            red: "#F64A4A",
+            white: "#E6E5EA",
+            green: "#A4FFAF",
+            orange: "#FB7C58",
+            yellow: "#F8CD65",
+            transparent: "transparent",
+            gray: {
+                500: "#817D92",
+                600: "#24232C",
+                700: "#18171F",
+                800: "#14131B",
+            },
+        },
+        fontSize: {
+            300: ["0.875rem", "1.25"],
+            400: ["1rem", "1.313rem"],
+            500: ["1.125rem", "1.5rem"],
+            600: ["1.5rem", "2rem"],
+            700: ["2rem", "2.625rem"],
+        },
+        screens: {
+            xs: "345px",
+            sm: "390px",
+            md: "640px",
+        },
     },
     plugins: [],
 };
