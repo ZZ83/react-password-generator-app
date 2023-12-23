346b6af147798c3287e19001fa578eb41cd0705b
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-22 21:27:13 -0800

    Add div element and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ function App() {
                 <h1 className="pt-16 text-center text-gray-500 md:pt-32 md:text-600">Password Generator</h1>
                 <main>
                     <Password />
+                    <div className="bg-gray-600 px-4 pb-4 pt-4 md:px-8 md:pb-8 md:pt-6"></div>
                 </main>
             </div>
         </div>
