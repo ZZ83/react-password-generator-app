9bc3dc111c21fabdfd34ff94e6e28e8c9ba09906
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-22 21:13:13 -0800

    Add wrapper div styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import Password from "./Password";
 
 function App() {
-    return <div className="h-screen w-full bg-gray-800 font-jetBrainsMono text-400 text-white"></div>;
+    return (
+        <div className="h-screen w-full bg-gray-800 font-jetBrainsMono text-400 text-white">
+            <div className="mx-auto max-w-[35.75rem] px-4"></div>
+        </div>
+    );
 }
 
 export default App;
