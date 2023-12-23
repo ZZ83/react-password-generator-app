fd7d1380fb3d4a63b6e3a8da0925a85769836c91
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-22 21:14:09 -0800

    Add main heading element and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,9 @@ import Password from "./Password";
 function App() {
     return (
         <div className="h-screen w-full bg-gray-800 font-jetBrainsMono text-400 text-white">
-            <div className="mx-auto max-w-[35.75rem] px-4"></div>
+            <div className="mx-auto max-w-[35.75rem] px-4">
+                <h1 className="pt-16 text-center text-gray-500 md:pt-32 md:text-600">Password Generator</h1>
+            </div>
         </div>
     );
 }
