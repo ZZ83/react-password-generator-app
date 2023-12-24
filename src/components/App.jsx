99995db1b1b6fdd45e3f3b8bea315f742e568e35
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2023-12-23 20:40:48 -0800

    Import CharaterLength component into App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Password from "./Password";
+import CharaterLength from "./CharaterLength";
 
 function App() {
+    const [charaterLength, setCharaterLength] = useState(10);
     return (
         <div className="h-screen w-full bg-gray-800 font-jetBrainsMono text-400 text-white">
             <div className="mx-auto max-w-[35.75rem] px-4">
                 <h1 className="pt-16 text-center text-gray-500 md:pt-32 md:text-600">Password Generator</h1>
                 <main>
                     <Password />
-                    <div className="bg-gray-600 px-4 pb-4 pt-4 md:px-8 md:pb-8 md:pt-6"></div>
+                    <div className="bg-gray-600 px-4 pb-4 pt-4 md:px-8 md:pb-8 md:pt-6">
+                        <CharaterLength charaterLength={charaterLength} />
+                    </div>
                 </main>
             </div>
         </div>
