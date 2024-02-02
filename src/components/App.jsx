9d47c20d37d0167e4669b2e30095f204d10c290d
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2024-02-01 21:55:54 -0800

    Fix background color not filling full height

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -53,7 +53,7 @@ function App() {
     }
 
     return (
-        <div className="h-screen w-full bg-gray-800 font-jetBrainsMono text-400 text-white">
+        <div className="min-h-full w-full bg-gray-800 pb-16 font-jetBrainsMono text-400 text-white">
             <div className="mx-auto max-w-[35.75rem] px-4">
                 <h1 className="pt-16 text-center text-gray-500 md:pt-32 md:text-600">Password Generator</h1>
                 <main>
