a146e4a30254e1c3d15d09d23b9de285f3bd2478
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2024-02-07 20:22:42 -0800

    Update password strength logic

--- src/components/PasswordStrengthMeter.jsx
+++ src/components/PasswordStrengthMeter.jsx
@@ -1,4 +1,19 @@
 function PasswordStrengthMeter({ password }) {
+    let strength = 0;
+
+    if (password.length > 8) {
+        strength += 1;
+    }
+    if (password.match(/[a-z]/) && password.match(/[A-Z]/)) {
+        strength += 1;
+    }
+    if (password.match(/\d/)) {
+        strength += 1;
+    }
+    if (password.match(/[^a-zA-Z\d]/)) {
+        strength += 1;
+    }
+
     if (password === "") {
         return (
             <div className="flex items-center gap-2 sm:gap-4 md:h-8">
@@ -10,7 +25,7 @@ function PasswordStrengthMeter({ password }) {
                 </div>
             </div>
         );
-    } else if (password.length >= 11) {
+    } else if (strength === 4) {
         return (
             <div className="flex items-center gap-2 sm:gap-4 md:h-8">
                 <span className="text-400 uppercase xs:text-500 md:text-600">Strong</span>
@@ -22,7 +37,7 @@ function PasswordStrengthMeter({ password }) {
                 </div>
             </div>
         );
-    } else if (password.length >= 9) {
+    } else if (strength === 3) {
         return (
             <div className="flex items-center gap-2 sm:gap-4 md:h-8">
                 <span className="text-400 uppercase xs:text-500 md:text-600">Medium</span>
@@ -34,11 +49,10 @@ function PasswordStrengthMeter({ password }) {
                 </div>
             </div>
         );
-    } else if (password.length >= 6) {
+    } else if (strength === 2) {
         return (
             <div className="flex items-center gap-2 sm:gap-4 md:h-8">
                 <span className="text-400 uppercase xs:text-500 md:text-600">Weak</span>
-
                 <div className="flex gap-1 xs:gap-2">
                     <div className="h-7 w-2.5 bg-orange"></div>
                     <div className="h-7 w-2.5 bg-orange"></div>
